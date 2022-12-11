f9d0766553fa764fa465f4bcdf541d9373d83f8e
Justin Dela Cruz <Traxiliongames@gmail.com>
2022-12-11 12:21:42 -0500

    Applied a Load Manager for accounts and signifiers for  C-S and S-C

--- Assets/NetworkedServer.cs
+++ Assets/NetworkedServer.cs
@@ -26,7 +26,9 @@ public class NetworkedServer : MonoBehaviour
         unreliableChannelID = config.AddChannel(QosType.Unreliable);
         HostTopology topology = new HostTopology(config, maxConnections);
         hostID = NetworkTransport.AddHost(topology, socketPort, null);
-        
+        playerAccounts = new LinkedList<PlayerAccount>();
+        LoadPlayerManagementFile();
+
     }
 
     // Update is called once per frame
@@ -60,32 +62,61 @@ public class NetworkedServer : MonoBehaviour
         }
 
     }
-  
+
     public void SendMessageToClient(string msg, int id)
     {
         byte error = 0;
         byte[] buffer = Encoding.Unicode.GetBytes(msg);
         NetworkTransport.Send(hostID, id, reliableChannelID, buffer, msg.Length * sizeof(char), out error);
     }
-    
+
     private void ProcessRecievedMsg(string msg, int id)
     {
         Debug.Log("msg recieved = " + msg + ".  connection id = " + id);
     }
-
-}
-
-public class PlayerAccount
-{
-    public const int PlayerIDSinifier = 1;
-    public string name, password;
-    public int id;
-
-    public PlayerAccount(int i, string n, string p)
+    public void LoadPlayerManagementFile()
     {
-        id = i;
-        name = n;
-        password = p;
-    }
+        if (File.Exists(Application.dataPath + Path.DirectorySeparatorChar + "PlayerManagementFile.txt"))
+        {
+            StreamReader sr = new StreamReader(Application.dataPath + Path.DirectorySeparatorChar + "PlayerManagementFile.txt");
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] csv = line.Split(',');
 
+                int signifier = int.Parse(csv[0]);
+                if (signifier == PlayerAccount.PlayerIDSinifier)
+                {
+                    playerAccounts.AddLast(new PlayerAccount(int.Parse(csv[1]), csv[2], csv[3]));
+                }
+            }
+        }
+    }
+    public static class ClientToServerSignifiers
+    {
+        public const int CreateAccount = 1;
+        public const int Login = 2;
+    }
+    public static class ServerToClientSignifiers
+    {
+        public const int LoginComplete = 1;
+        public const int LoginFailed = 2;
+        public const int AccountCreationComplete = 3;
+        public const int AccountCreationFailed = 4;
+    }
+    public class PlayerAccount
+    {
+        public const int PlayerIDSinifier = 1;
+        public string name, password;
+        public int id;
+        public PlayerAccount(int i, string n, string p)
+        {
+            id = i;
+            name = n;
+            password = p;
+        }
+
+    }
 }
+
+
